7ad4fe3d3bc539953a633b903e9a7a2515d20670
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-01 23:49:59 -0300

    fix: Muda nomes do Model e adiciona campo de estoque

--- Models/Products.cs
+++ Models/Products.cs
@@ -5,22 +5,25 @@ namespace CrudDotnet.Models
     public class Product
     {
         [Key]
-        public int Id { get; set; }
+        public int Código { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [MaxLength(40, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
         [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
 
-        public string Title { get; set; }
+        public string Produto { get; set; }
 
         [MaxLength(250, ErrorMessage = "Este campo deve conter no máximo 250 caracteres")]
 
-        public string Description { get; set; }
+        public string Descricao { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
 
-        public decimal Price { get; set; }
+        public decimal Estoque { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public decimal Preco { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
